4cb51814a5d26c6747ef15fc956343e8d7536f5b
kuadam <kuadam2@gmail.com>
2020-04-03 16:26:05 +0200

    lab5 MDBLab

---
+++ MDBLab/MDBLab-ejb/build/generated-sources/ap-source-output/ejb/NewsItem_.java
@@ -0,0 +1,15 @@
+package ejb;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-03T16:21:59")
+@StaticMetamodel(NewsItem.class)
+public class NewsItem_ { 
+
+    public static volatile SingularAttribute<NewsItem, String> heading;
+    public static volatile SingularAttribute<NewsItem, Long> id;
+    public static volatile SingularAttribute<NewsItem, String> body;
+
+}
