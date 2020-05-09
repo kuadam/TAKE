56af934b0a8a1945de4d2d16cf4010cce66482b3
kuadam <kuadam2@gmail.com>
2020-05-09 16:59:53 +0200

    lab8 ASP.NET MVC + Entity Framework (Code First)

---
+++ MvcNews/MvcNews/Data/NewsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcNews.Models;
+
+namespace MvcNews.Data
+{
+	public class NewsDbContext : DbContext
+	{
+		public NewsDbContext(DbContextOptions<NewsDbContext> options) : base(options) { }
+
+		public DbSet<NewsItem> News { get; set; }
+	}
+}
---
+++ MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcNews.Models
+{
+	public class NewsItem
+	{
+		public int Id { get; set; }
+
+		[DataType(DataType.DateTime)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
+		public DateTime TimeStamp { get; set; }
+
+		[Required]
+		[StringLength(140, MinimumLength = 5)]
+		public string Text { get; set; }
+
+		[Timestamp]
+		public byte[] RowVersion { get; set; }
+	}
+}
